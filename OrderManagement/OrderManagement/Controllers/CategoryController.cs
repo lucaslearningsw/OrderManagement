c95474c9e92a0dbc974a5323e89e46dbb9386e88
lucas.alixandre <lucas.alixandre@maisinteligencia.com.br>
2022-01-21 17:11:32 -0300

    created DbContext, Model Category and view to filled view with data model

---
+++ OrderManagement/OrderManagement/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OrderManagement.Data;
+using OrderManagement.Models;
+
+namespace OrderManagement.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db; 
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}
---
+++ OrderManagement/OrderManagement/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Models;
+
+namespace OrderManagement.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public  ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Category { set; get; }
+    }
+}
