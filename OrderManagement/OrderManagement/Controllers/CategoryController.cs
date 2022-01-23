c3fc987c77acbd2dadd5fee710a2a5bf4501ca65
lucas.alixandre <lucas.alixandre@maisinteligencia.com.br>
2022-01-23 10:43:48 -0300

    added delete Category in Controller

--- OrderManagement/OrderManagement/Controllers/CategoryController.cs
+++ OrderManagement/OrderManagement/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace OrderManagement.Controllers
 
         public CategoryController(ApplicationDBContext db)
         {
-            _db = db; 
+            _db = db;
         }
         public IActionResult Index()
         {
@@ -41,7 +41,7 @@ namespace OrderManagement.Controllers
 
         public IActionResult Edit(int? id)
         {
-            if(id==null || id==0)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
@@ -54,5 +54,54 @@ namespace OrderManagement.Controllers
 
             return View(obj);
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
     }
+
 }
